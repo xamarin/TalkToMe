f4a0549526e55d9c277397fb77e556534248fc5a
Andrew Ditmer <andrew@ditmer.us>
2015-08-20 20:03:39 -0700

    Initial Commit

---
+++ TalkToMe/TalkToMe.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TalkToMe.WinPhone.Resources;
+
+namespace TalkToMe.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
