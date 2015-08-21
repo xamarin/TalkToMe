a38355c31163faab9b892cba427620f3a63340dd
Andrew Ditmer <andrew@ditmer.us>
2015-08-20 20:13:23 -0700

    Build in Text-to-Speech and Geolocation Features

--- TalkToMe/TalkToMe/App.cs
+++ TalkToMe/TalkToMe/App.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Refractored.Xam.TTS;
+using Geolocator.Plugin;
 
 namespace TalkToMe
 {
@@ -12,16 +14,35 @@ namespace TalkToMe
         public App()
         {
             // The root page of your application
+            var btnTalk = new Button();
+
+            var lblLocation = new Label();
+
+            btnTalk.Text = "Talk to me";
+            btnTalk.BackgroundColor = Color.Gray;
+            btnTalk.TextColor = Color.Green;
+
+            btnTalk.Clicked += async (sender, args) =>
+            {
+                //lblLocation.Text = "You clicked?";
+
+                CrossTextToSpeech.Current.Speak("You have been rerouted through the Lincoln Tunnel.");
+
+                var location = CrossGeolocator.Current;
+
+                var position = await location.GetPositionAsync(timeout: 10000);
+
+                lblLocation.Text = $"Lat: {position.Latitude} Long: {position.Longitude}";
+            };
+
             MainPage = new ContentPage
             {
                 Content = new StackLayout
                 {
                     VerticalOptions = LayoutOptions.Center,
                     Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
+                        btnTalk,
+                        lblLocation
                     }
                 }
             };
